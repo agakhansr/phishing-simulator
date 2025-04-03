d9a04b3b08fb25e457d45af0807ce353d8764b89
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2025-04-03 16:15:35 +0400

    removed redundant endpoints

--- phishing-management-api/src/phishing/phishing.controller.ts
+++ phishing-management-api/src/phishing/phishing.controller.ts
@@ -1,22 +1,11 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { PhishingService } from './phishing.service';
-import { CreatePhishingDto } from './dto/create-phishing.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { CurrentUser } from '../auth/decorators/current-user.decorator';
-import { User } from '../users/schemas/user.schema';
 
 @Controller('phishing')
 @UseGuards(JwtAuthGuard)
 export class PhishingController {
   constructor(private readonly phishingService: PhishingService) {}
-
-  @Post()
-  async create(
-    @Body() createPhishingDto: CreatePhishingDto,
-    @CurrentUser() user: User,
-  ) {
-    return this.phishingService.create(createPhishingDto, user);
-  }
 
   @Get()
   async findAll() {
@@ -26,10 +15,5 @@ export class PhishingController {
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.phishingService.findOne(id);
-  }
-
-  @Post(':id/send')
-  async sendPhishingEmail(@Param('id') id: string) {
-    return this.phishingService.sendPhishingEmail(id);
   }
 }
